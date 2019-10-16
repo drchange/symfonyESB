aaa40910e44ee949e22293d0c935851205173966
Willy DAMTCHOU <willy.damtchou@afrikpay.com>
2019-10-16 11:06:21 +0100

    ssl

--- src/Service/HttpCurlClientService.php
+++ src/Service/HttpCurlClientService.php
@@ -10,12 +10,16 @@ use \Exception;
 class HttpCurlClientService
 {
 
-    public function push(string $url, $data, string $method = 'GET', $type = 'json', $headers = []) : string
+    public function push(string $url, array $data, string $method = 'GET', $type = 'json', array $params=[], array $headers = []) : string
     {
+        $data = str_replace("\n", "", $data);
+        $data = str_replace("\r", "", $data);
+        $data = str_replace("\t", "", $data);
+
         return $this->{"send$method"}($url, $data, $type, $headers);
     }
     
-    public function sendGET(string $url, $data) : string
+    public function sendGET(string $url, array $data, string $type = 'json', array $params=[], array $headers = []) : string
     {
         $httpClient = new CurlHttpClient();
         $response = $httpClient->request('GET', $url, [
@@ -31,7 +35,7 @@ class HttpCurlClientService
     }
     
     // type = "json/xml/raw data"
-    public function sendPOST(string $url, $data, $type = 'json', $headers = []) : string
+    public function sendPOST(string $url, array $data, string $type = 'json', array $params=[], array $headers = []) : string
     {
         $httpClient = new CurlHttpClient();
         $response = "";
@@ -42,33 +46,15 @@ class HttpCurlClientService
                 'headers' => $headers
             ]);
         } elseif (true) {
-            
-            $data = str_replace("\n", "", $data);
-            $data = str_replace("\t", "", $data);
-            $data = str_replace("\r", "", $data);
-
-            $header = array(
-                "Content-type: text/xml;charset=\"utf-8\"",
-                "Accept: text/xml",
-                "Cache-Control: no-cache",
-                "Pragma: no-cache",
-                "Content-length: " . strlen($data)
-            );
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 120);
-            curl_setopt($ch, CURLOPT_TIMEOUT, 150);
-            curl_setopt($ch, CURLOPT_URL, $url);
-            curl_setopt($ch, CURLOPT_POST, true);
-            curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-            $response = curl_exec($ch);
+            $bodyparam = $dataparam = 'body';
+            $response = $httpClient->request('POST', $url, [
+                $bodyparam => $type,
+                $dataparam => $data,
+                'headers' => $headers,
+                'verify_host' => false,
+                'verify_peer' => false,
+            ]);
         }
         return $response->getContent();
     }
-
-    
-    
 }
