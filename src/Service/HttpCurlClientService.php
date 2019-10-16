0a2a15e8ad334e9ca1c82ffa4e2a3087cf2341a8
Willy DAMTCHOU <willy.damtchou@afrikpay.com>
2019-10-16 10:27:19 +0100

    ssl

--- src/Service/HttpCurlClientService.php
+++ src/Service/HttpCurlClientService.php
@@ -43,14 +43,17 @@ class HttpCurlClientService
                 'headers' => $headers
             ]);
         } elseif (true) {
-            $bodyparam = $dataparam = 'body';
-            $response = $httpClient->request('POST', $url, [
-                $bodyparam => $type,
-                $dataparam => $data,
-                'headers' => $headers,
-                'verify_host' => false,
-                'verify_peer' => false,
-            ]);
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 120);
+            curl_setopt($ch, CURLOPT_TIMEOUT, 150);
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+            $response = curl_exec($ch);
         }
         return $response->getContent();
     }
